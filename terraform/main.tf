d9ff984267e21383202d09c1c06337fffafac83a
andrey-an <gbox8048@gmail.com>
2020-04-29 21:22:51 +0300

    terraform 12

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,31 @@
 terraform {
   # Версия terraform
-  #required_version = "0.11.14"
-  required_version = "< 0.12.0"
+  required_version = ">= 0.12"
 }
 
 provider "google" {
   # Версия провайдера
-  version = "2.0.0"
+  version = "~> 2"
 
   # ID проекта
-  project = "${var.project}"
-  region  = "${var.region}"
+  project = "var.project"
+  region  = "var.region"
 }
 
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
-  zone         = "${var.region}"
+  zone         = "var.region"
   tags         = ["reddit-app"]
 
   connection {
     type  = "ssh"
     user  = "master"
     agent = false
+    host = "self.public_ip"
 
     # путь до приватного ключа
-    private_key = "${file(var.private_key_path)}"
+    private_key = "file(var.private_key_path)"
   }
 
   provisioner "file" {
@@ -38,14 +38,14 @@ resource "google_compute_instance" "app" {
   }
 
   # определение загрузочного диска
-  metadata {
+  metadata = {
     # путь до публичного ключа
-    ssh-keys = "${var.ssh_user}:${file(var.ssh_key)}${var.ssh_user1}:${file(var.ssh_key1)}"
+    ssh-keys = "var.ssh_user:file(var.ssh_key)var.ssh_user1:file(var.ssh_key1)"
   }
 
   boot_disk {
     initialize_params {
-      image = "${var.disk_image}"
+      image = "var.disk_image"
     }
   }
 
@@ -55,7 +55,8 @@ resource "google_compute_instance" "app" {
     network = "default"
 
     # использовать ephemeral IP для доступа из Интернет
-    access_config {}
+    access_config {
+    }
   }
 }
 
@@ -77,3 +78,4 @@ resource "google_compute_firewall" "firewall_puma" {
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["reddit-app"]
 }
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,40 +1,41 @@
-variable project {
+variable "project" {
   description = "Project ID"
 }
 
-variable region {
+variable "region" {
   description = "Region"
 
   # Значение по умолчанию
   default = "europe-west1-b"
 }
 
-variable public_key_path {
+variable "public_key_path" {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
 
-variable private_key_path {
+variable "private_key_path" {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
 
-variable disk_image {
+variable "disk_image" {
   description = "Disk image"
 }
 
-variable ssh_user {
+variable "ssh_user" {
   description = "User"
 }
 
-variable ssh_key {
+variable "ssh_key" {
   description = "User"
 }
 
-variable ssh_user1 {
+variable "ssh_user1" {
   description = "User"
 }
 
-variable ssh_key1 {
+variable "ssh_key1" {
   description = "User"
 }
+
