bf5813a8cb75da2a1d0bc661c39f5941a1647245
andrey-an <gbox8048@gmail.com>
2020-05-15 00:18:27 +0300

    home tast #11

--- terraform/prod/output.tf
+++ terraform/prod/output.tf
@@ -6,3 +6,11 @@ output "app_external_ip_db" {
   value = module.db.app_external_ip
 }
 
+output "app_internal_ip" {
+  value = module.app.app_internal_ip
+}
+
+output "app_internal_ip_db" {
+  value = module.db.app_internal_ip
+}
+
